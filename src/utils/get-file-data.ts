79aac10b388796c2e67ae49652c9277d0666e00f
Hoang Nguyen <hoangncd3@gmail.com>
2024-06-07 16:33:29 +0700

    fix: log base path

--- src/utils/get-file-data.ts
+++ src/utils/get-file-data.ts
@@ -3,6 +3,9 @@ import { promises as fs } from 'fs'
 import { logger } from '@/configs/logger'
 
 export const getFileData = async (filePath: string): Promise<string> => {
+  // eslint-disable-next-line no-console
+  console.log(process.cwd())
+
   try {
     const fileData = await fs.readFile(process.cwd() + filePath, 'utf-8')
     return fileData
